25384368e07c63ee6c0c848b0b7fa46a822f79c5
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-03 20:27:06 +0600

    flashcardData added

---
+++ CodeAlpha_FlashcardQuizApp/lib/data/flashcard_data.dart
@@ -0,0 +1,33 @@
+import '../models/flashcard_model.dart';
+
+List<Flashcard> flashcards = [
+  Flashcard(
+    question: "What is Flutter?",
+    answer: "Flutter is Google's UI toolkit for building cross-platform applications.",
+  ),
+
+  Flashcard(
+    question: "What is Dart?",
+    answer: "Dart is the programming language used by Flutter.",
+  ),
+
+  Flashcard(
+    question: "What is a Widget in Flutter?",
+    answer: "A Widget is a building block of a Flutter user interface.",
+  ),
+
+  Flashcard(
+    question: "What is StatefulWidget?",
+    answer: "StatefulWidget is a widget whose state can change during the lifetime of the widget.",
+  ),
+
+  Flashcard(
+    question: "What is StatelessWidget?",
+    answer: "StatelessWidget is a widget that does not have mutable state.",
+  ),
+
+  Flashcard(
+    question: "What is API?",
+    answer: "API allows different software applications to communicate with each other.",
+  ),
+];
