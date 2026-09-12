fd4dfaa5dc05771b0f775eb15ba9dc90d9846eb8
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-12 12:42:02 +0600

    added splash Screen

---
+++ CodeAlpha_RandomQuoteGenerator/lib/screens/splash_screen.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import '../utils/asset_path.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 3),
+          () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF16002B),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Image.asset(Urls.logo,
+              width: 220,
+              height: 220,
+            ),
+
+            const SizedBox(height: 25),
+
+            const Text(
+              "Random Quotes",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+
+            const Text(
+              "Inspiration in every word",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 15,
+              ),
+            ),
+
+            const SizedBox(height: 35),
+
+            const SizedBox(
+              width: 35,
+              height: 35,
+              child: CircularProgressIndicator(
+                strokeWidth: 3,
+                color: Colors.amber,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
